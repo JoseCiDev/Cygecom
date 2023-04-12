eebc19570e5c55576159001ad3575b199790970d
Cesar Sousa <cesar.luiz@essentia.com.br>
2023-04-12 17:52:42 -0300

    FIX: container vite port 5173

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+ 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+    },
+
     plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+        laravel([
+            'resources/sass/app.scss',
+            'resources/js/app.js',
+        ]),
     ],
 });
