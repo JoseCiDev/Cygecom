639033ab76524b42ed176f4ce38e738a44e9c866
Luis Pedro Piuma <luis.pedro@essentia.com.br>
2023-04-20 18:14:03 -0300

    Adição de asset em todas importações de app para torna caminho absoluto invés de relativo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,70 +12,70 @@
 	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 
 	<!-- Bootstrap -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
 	<!-- jQuery UI -->
-	<link rel="stylesheet" href="css/plugins/jquery-ui/jquery-ui.min.css">
+	<link rel="stylesheet" href="{{asset('css/plugins/jquery-ui/jquery-ui.min.css')}}">
 	<!-- PageGuide -->
-	<link rel="stylesheet" href="css/plugins/pageguide/pageguide.css">
+	<link rel="stylesheet" href="{{asset('css/plugins/pageguide/pageguide.css')}}">
 	<!-- Fullcalendar -->
-	<link rel="stylesheet" href="css/plugins/fullcalendar/fullcalendar.css">
-	<link rel="stylesheet" href="css/plugins/fullcalendar/fullcalendar.print.css" media="print">
+	<link rel="stylesheet" href="{{asset('css/plugins/fullcalendar/fullcalendar.css')}}">
+	<link rel="stylesheet" href="{{asset('css/plugins/fullcalendar/fullcalendar.print.css')}}" media="print">
 	<!-- chosen -->
-	<link rel="stylesheet" href="css/plugins/chosen/chosen.css">
+	<link rel="stylesheet" href="{{asset('css/plugins/chosen/chosen.css')}}">
 	<!-- select2 -->
-	<link rel="stylesheet" href="css/plugins/select2/select2.css">
+	<link rel="stylesheet" href="{{asset('css/plugins/select2/select2.css')}}">
 	<!-- icheck -->
-	<link rel="stylesheet" href="css/plugins/icheck/all.css">
+	<link rel="stylesheet" href="{{asset('css/plugins/icheck/all.css')}}">
 	<!-- Theme CSS -->
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="{{asset('css/style.css')}}">
 	<!-- Color CSS -->
-	<link rel="stylesheet" href="css/themes.css">
+	<link rel="stylesheet" href="{{asset('css/themes.css')}}">
 
 	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
+	<script src="{{asset('js/jquery.min.js')}}"></script>
 
 
 	<!-- Nice Scroll -->
-	<script src="js/plugins/nicescroll/jquery.nicescroll.min.js"></script>
+	{{-- <script src="js/plugins/nicescroll/jquery.nicescroll.min.js"></script> --}}
 	<!-- jQuery UI -->
-	<script src="js/plugins/jquery-ui/jquery-ui.js"></script>
+	<script src="{{asset('js/plugins/jquery-ui/jquery-ui.js')}}"></script>
 	<!-- Touch enable for jquery UI -->
-	<script src="js/plugins/touch-punch/jquery.touch-punch.min.js"></script>
+	<script src="{{asset('js/plugins/touch-punch/jquery.touch-punch.min.js')}}"></script>
 	<!-- slimScroll -->
-	<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="{{asset('js/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
 	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
+	<script src="{{asset('js/bootstrap.min.js')}}"></script>
 	<!-- vmap -->
-	<script src="js/plugins/vmap/jquery.vmap.min.js"></script>
-	<script src="js/plugins/vmap/jquery.vmap.world.js"></script>
-	<script src="js/plugins/vmap/jquery.vmap.sampledata.js"></script>
+	<script src="{{asset('js/plugins/vmap/jquery.vmap.min.js')}}"></script>
+	<script src="{{asset('js/plugins/vmap/jquery.vmap.world.js')}}"></script>
+	<script src="{{asset('js/plugins/vmap/jquery.vmap.sampledata.js')}}"></script>
 	<!-- Bootbox -->
-	<script src="js/plugins/bootbox/jquery.bootbox.js"></script>
+	<script src="{{asset('js/plugins/bootbox/jquery.bootbox.js')}}"></script>
 	<!-- Flot -->
-	<script src="js/plugins/flot/jquery.flot.min.js"></script>
-	<script src="js/plugins/flot/jquery.flot.bar.order.min.js"></script>
-	<script src="js/plugins/flot/jquery.flot.pie.min.js"></script>
-	<script src="js/plugins/flot/jquery.flot.resize.min.js"></script>
+	<script src="{{asset('js/plugins/flot/jquery.flot.min.js')}}"></script>
+	<script src="{{asset('js/plugins/flot/jquery.flot.bar.order.min.js')}}"></script>
+	<script src="{{asset('js/plugins/flot/jquery.flot.pie.min.js')}}"></script>
+	<script src="{{asset('js/plugins/flot/jquery.flot.resize.min.js')}}"></script>
 	<!-- imagesLoaded -->
-	<script src="js/plugins/imagesLoaded/jquery.imagesloaded.min.js"></script>
+	<script src="{{asset('js/plugins/imagesLoaded/jquery.imagesloaded.min.js')}}"></script>
 	<!-- PageGuide -->
-	<script src="js/plugins/pageguide/jquery.pageguide.js"></script>
+	<script src="{{asset('js/plugins/pageguide/jquery.pageguide.js')}}"></script>
 	<!-- FullCalendar -->
-	<script src="js/plugins/fullcalendar/moment.min.js"></script>
-	<script src="js/plugins/fullcalendar/fullcalendar.min.js"></script>
+	<script src="{{asset('js/plugins/fullcalendar/moment.min.js')}}"></script>
+	<script src="{{asset('js/plugins/fullcalendar/fullcalendar.min.js')}}"></script>
 	<!-- Chosen -->
-	<script src="js/plugins/chosen/chosen.jquery.min.js"></script>
+	<script src="{{asset('js/plugins/chosen/chosen.jquery.min.js')}}"></script>
 	<!-- select2 -->
-	<script src="js/plugins/select2/select2.min.js"></script>
+	<script src="{{asset('js/plugins/select2/select2.min.js')}}"></script>
 	<!-- icheck -->
-	<script src="js/plugins/icheck/jquery.icheck.min.js"></script>
+	<script src="{{asset('js/plugins/icheck/jquery.icheck.min.js')}}"></script>
 
 	<!-- Theme framework -->
-	<script src="js/eakroko.min.js"></script>
+	<script src="{{asset('js/eakroko.min.js')}}"></script>
 	<!-- Theme scripts -->
-	<script src="js/application.min.js"></script>
+	<script src="{{asset('js/application.min.js')}}"></script>
 	<!-- Just for demonstration -->
-	<script src="js/demonstration.min.js"></script>
+	<script src="{{asset('js/demonstration.min.js')}}"></script>
 
 	<!--[if lte IE 9]>
 		<script src="js/plugins/placeholder/jquery.placeholder.min.js"></script>
@@ -89,7 +89,7 @@
 	<!-- Favicon -->
 	{{-- <link rel="shortcut icon" href="img/favicon.ico" /> --}}
 	<!-- Apple devices Homescreen icon -->
-	<link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-precomposed.png" />
+	<link rel="{{asset('apple-touch-icon-precomposed')}}" href="img/apple-touch-icon-precomposed.png" />
 
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
 </head>
