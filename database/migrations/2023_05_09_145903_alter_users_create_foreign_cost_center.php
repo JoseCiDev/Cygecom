bd4ac93d81489506406b1e435b45e159401d69f1
Cesar Sousa <cesar.luiz@essentia.com.br>
2023-05-09 15:15:44 -0300

    ED-894 tabela cost_centeres criada

---
+++ database/migrations/2023_05_09_145903_alter_users_create_foreign_cost_center.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('cost_center_id')->nullable();
+            $table->foreign('cost_center_id')->references('id')->on('cost_centers');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['cost_center_id']);
+            $table->dropColumn('cost_center_id');
+        });
+    }
+};
