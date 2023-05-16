f9f0ac7099e81a793bf8a93123255bc36c97688d
Luis Pedro <luis.pedro@essentia.com.br>
2023-05-16 11:11:26 -0300

    ED-933 Ajustes de interfaces e remoção de classe não usada de componente

--- app/Contracts/EmailControllerInterface.php
+++ app/Contracts/EmailControllerInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Contracts;
 
 use App\Providers\UserService;
 use Illuminate\Http\RedirectResponse;
--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Contracts\EmailControllerInterface;
 use App\Http\Controllers\Controller;
 use App\Mail\GenericEmail;
 use App\Providers\UserService;
