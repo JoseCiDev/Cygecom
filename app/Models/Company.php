d6c15397f117ae9483afba8a51f2ae30e560a3af
Cesar Sousa <cesar.luiz@essentia.com.br>
2023-05-30 09:54:19 -0300

    soft deletes company model excluido

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,12 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\{Model, SoftDeletes};
+use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
     use HasFactory;
-    use SoftDeletes;
 
     public function costCenter()
     {
