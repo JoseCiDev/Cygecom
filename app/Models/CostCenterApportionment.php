43dbb58cde40b9ac7a40802c2c47d6fcc72d7a07
Luis Pedro <luis.pedro@essentia.com.br>
2023-06-06 18:33:36 -0300

    ED-983 Ajuste do Model QuoteRequest e criação dos Models CostCenterApportionment e QuoteRequestFile

---
+++ app/Models/CostCenterApportionment.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CostCenterApportionment extends Model
+{
+    use HasFactory;
+
+    public function costCenter()
+    {
+        return $this->belongsTo(CostCenter::class, 'cost_center_id');
+    }
+
+    public function quoteRequest()
+    {
+        return $this->belongsTo(QuoteRequest::class, 'quote_request_id');
+    }
+
+    public function deletedByUser()
+    {
+        return $this->belongsTo(User::class, 'deleted_by');
+    }
+    public function updatedByUser()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    protected $fillable = [
+        'apportionment_percentage',
+        'apportionment_value',
+        'quote_request_id',
+        'cost_center_id',
+        'updated_by',
+        'deleted_by',
+        'deleted_at'
+    ];
+}
