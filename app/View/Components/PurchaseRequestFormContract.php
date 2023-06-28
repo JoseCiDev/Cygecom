3912847831e9d45c65d21c7e980b29e7b397fd5a
Luis Pedro <luis.pedro@essentia.com.br>
2023-06-28 14:26:50 -0300

    ED-1028 Criação do formulários/componentes Contrato, Serviço e Produto

---
+++ app/View/Components/PurchaseRequestFormContract.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\Company;
+use App\Models\CostCenter;
+use App\Models\Supplier;
+use App\Providers\PurchaseRequestService;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class PurchaseRequestFormContract extends Component
+{
+    private $purchaseRequestService;
+
+    private $id;
+
+    private $isCopy;
+
+    public function __construct(PurchaseRequestService $purchaseRequestService, int $id = null, $isCopy = false)
+    {
+        $this->purchaseRequestService = $purchaseRequestService;
+        $this->isCopy              = $isCopy;
+        $this->id                  = $id;
+    }
+
+    public function render(): View|Closure|string
+    {
+        $companies       = Company::all();
+        $suppliers = Supplier::all();
+        $userCostCenters = auth()->user()->userCostCenterPermission;
+        $costCenters     = CostCenter::whereIn('id', $userCostCenters->pluck('cost_center_id'))->get();
+
+        $params = ["companies" => $companies, "costCenters" => $costCenters, 'suppliers' => $suppliers];
+
+        if ($this->id) {
+            $params['id']           = $this->id;
+            $params['purchaseRequest'] = $this->purchaseRequestService->purchaseRequestById($this->id);
+            $params['isCopy']       = $this->isCopy;
+        }
+
+        return view('components.purchase-request.purchase-request-form-contract', $params);
+    }
+}
