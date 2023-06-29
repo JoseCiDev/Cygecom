36d43c29e8b5e4a30c53fb4608d68d753e5f82b3
Luis Pedro <luis.pedro@essentia.com.br>
2023-06-29 17:32:12 -0300

    ED-1044 Criação de CLI para importar CSV. [php artisan csv-import <csv-path>]

---
+++ app/Console/Commands/ImportCSV.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Providers\CSVImporter;
+use Illuminate\Console\Command;
+
+class ImportCSV extends Command
+{
+    /**
+     * Nome e assinatura do comando com o caminho do arquivo csv de entrada
+     * @var string
+     */
+    protected $signature = 'csv-import {csv-path}'; // Defina o nome do seu comando aqui
+
+    /**
+     * @var string
+     */
+    protected $description = 'Lê um arquivo CSV, gera um array de dados, e executa um insert no banco de dados. É opcional passar um caminho de arquivo de saída para salvar o retorno do array';
+
+    /**
+     * Execute the console command.
+     *
+     */
+    public function handle()
+    {
+        $csvPath = $this->argument('csv-path');
+        preg_match('/\/([^\/]+)\.[^.]+$/', $csvPath, $matches);
+        $fileName = $matches[1] . time() . rand() . '.php';
+        $csvImporter = new CSVImporter($csvPath);
+        $outPath = "database/seeders/import/data/$fileName";
+        $csvImporter->generateArrayFile($outPath);
+
+        $this->info("Comando executado com sucesso! Verifique o diretório seeders/data o arquivo $fileName");
+    }
+}
