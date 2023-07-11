989c932e277d383f4a233663cfdbfca5093f6d5e
Luis Pedro <luis.pedro@essentia.com.br>
2023-07-11 14:53:53 -0300

    ED-1074 Criação de middleware para checar perfil do usuário

---
+++ app/Http/Middleware/CheckProfileMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckProfileMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next, $profile)
+    {
+        $isAdmin = $request->user()->profile->is_admin;
+
+        if ($isAdmin || $request->user()->profile->name === $profile) {
+            return $next($request);
+        }
+
+        abort(403, 'Acesso não autorizado.');
+    }
+}
