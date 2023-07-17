0339a6985d7035e8e83c93626ae71c896134d2cb
Luis Pedro <luis.pedro@essentia.com.br>
2023-07-17 16:52:01 -0300

    ED-1074 Melhorias no dashboard de suprimentos

--- resources/views/components/supplies/index.blade.php
+++ resources/views/components/supplies/index.blade.php
@@ -1,12 +1,12 @@
 <x-app>
     <x-slot name="title">
-        <h1>Página de suprimentos</h1>
+        <h1>Dashboard de suprimentos</h1>
     </x-slot>
     <div class="row">
         <div class="col-md-12 text-center">
             <div class="card">
                 <div class="card-body">
-                    <h2>Bem-vindo a tela de suprimentos!</h2>
+                    <h2>Bem-vindo ao dashboard de suprimentos!</h2>
                     <p>Este é o portal de gerenciamento de compras do grupo Essentia!</p>
                 </div>
             </div>
@@ -28,11 +28,9 @@
                                         <a href="{{route('supplies.product')}}" class="btn btn-grey-4">Ir para produtos</a>
                                     </div>
                                 </li>
-                                <li>Contratadação do tipo suprimentos: {{$productAcquiredBySuppliesQtd}}</li>
-                                <li>Quantidade de COMEX: {{$productComexQtd}}</li>
-                                <li>Unlimited Traffic</li>
-                                <li>Lorem ipsum dolor.</li>
-                                <li>Odio, fugit, nesciunt!</li>
+                                <li>Qtd. de contratações do tipo suprimentos: <strong>{{$productAcquiredBySuppliesQtd}}</strong></li>
+                                <li>Qtd. de COMEX: <strong>{{$productComexQtd}}</strong></li>
+                                <li>Qtd. desejadas p/ hoje: <strong>{{$productDesiredTodayQtd}}</strong></li>
                             </ul>
                             <ul class="pricing green col-sm-4">
                                 <li class="head">
@@ -42,11 +40,9 @@
                                         <a href="{{route('supplies.service')}}" class="btn btn-grey-4">Ir para serviços</a>
                                     </div>
                                 </li>
-                                <li>Contratadação do tipo suprimentos: {{$serviceAcquiredBySuppliesQtd}}</li>
-                                <li>Quantidade de COMEX: {{$serviceComexQtd}}</li>
-                                <li>Unlimited Traffic</li>
-                                <li>Lorem ipsum dolor.</li>
-                                <li>Odio, fugit, nesciunt!</li>
+                                <li>Qtd. de contratações do tipo suprimentos: <strong>{{$serviceAcquiredBySuppliesQtd}}</strong></li>
+                                <li>Qtd. de COMEX: <strong>{{$serviceComexQtd}}</strong></li>
+                                <li>Qtd. desejadas p/ hoje: <strong>{{$serviceDesiredTodayQtd}}</strong></li>
                             </ul>
                             <ul class="pricing red col-sm-4">
                                 <li class="head">
@@ -56,11 +52,9 @@
                                         <a href="{{route('supplies.contract')}}" class="btn btn-grey-4">Ir para contratos</a>
                                     </div>
                                 </li>
-                                <li>Contratadação do tipo suprimentos: {{$contractAcquiredBySuppliesQtd}}</li>
-                                <li>Quantidade de COMEX: {{$contractComexQtd}}</li>
-                                <li>Unlimited Traffic</li>
-                                <li>Lorem ipsum dolor.</li>
-                                <li>Odio, fugit, nesciunt!</li>
+                                <li>Qtd. de contratações do tipo suprimentos: <strong>{{$contractAcquiredBySuppliesQtd}}</strong></li>
+                                <li>Qtd. de COMEX: <strong>{{$contractComexQtd}}</strong></li>
+                                <li>Qtd. desejadas p/ hoje: <strong>{{$contractDesiredTodayQtd}}</strong></li>
                             </ul>
                         </div>
                     </div>
