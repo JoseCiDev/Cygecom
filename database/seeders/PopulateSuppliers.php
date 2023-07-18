94d04ebd9e206f0bf1ec723bf89314d495c11f9f
Luis Pedro <luis.pedro@essentia.com.br>
2023-07-18 17:05:51 -0300

    ED-1044 Melhorias no seeder PopulateSuppliers para validações e configurações iniciais de fornecedores

--- database/seeders/PopulateSuppliers.php
+++ database/seeders/PopulateSuppliers.php
@@ -5,12 +5,19 @@ namespace Database\Seeders;
 use App\Models\Address;
 use App\Models\Phone;
 use App\Models\Supplier;
+use App\Providers\ValidatorService;
 use ErrorException;
+use Illuminate\Database\QueryException;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class PopulateSuppliers extends Seeder
 {
+    public function __construct(private ValidatorService $validatorService)
+    {
+    }
+
     /**
      * @throws ErrorException
      */
@@ -20,24 +27,35 @@ class PopulateSuppliers extends Seeder
 
         if ($suppliersData) {
             DB::transaction(function () use ($suppliersData) {
-                foreach ($suppliersData as $data) {
-                    $phoneData = ['number' => $data['phone[number]']];
-                    $phone = Phone::create($phoneData);
+                try {
+                    foreach ($suppliersData as $data) {
+                        $validator = $this->validatorService->supplier($data);
+                        if ($validator->fails()) {
+                            continue;
+                        }
 
-                    $address = new Address;
-                    $fillableData = array_intersect_key($data, array_flip($address->getFillable()));
-                    $address->fill($fillableData);
-                    $address->save();
+                        if ((bool)$data['number']) {
+                            $phoneData = ['number' => $data['number'], 'phone_type' => 'commercial'];
+                            $phone = Phone::create($phoneData);
+                            $data['phone_id'] = $phone->id;
+                        }
 
-                    $data['phone_id'] = $phone->id;
-                    $data['address_id'] = $address->id;
+                        $address = new Address;
+                        $fillableData = array_intersect_key($data, array_flip($address->getFillable()));
+                        $address->fill($fillableData);
+                        $address->save();
 
-                    $supplier = new Supplier;
-                    $fillableData = array_intersect_key($data, array_flip($supplier->getFillable()));
-                    $supplier->fill($fillableData);
-                    $supplier->save();
+                        $data['address_id'] = $address->id;
 
-                    dd($supplier->toArray());
+                        $supplier = new Supplier;
+                        $fillableData = array_intersect_key($data, array_flip($supplier->getFillable()));
+                        $supplier->fill($fillableData);
+                        $supplier->qualification = 'qualificado';
+                        $supplier->save();
+                    }
+                } catch (QueryException $error) {
+                    DB::rollback();
+                    Log::error('Ocorreu um erro ao executar a transação: ' . $error->getMessage());
                 }
             });
         }
