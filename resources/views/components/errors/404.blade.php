eaa453740bc20c3566b73196e488ce9f47bca44d
Cesar Sousa <cesar.luiz@essentia.com.br>
2023-07-19 16:26:27 -0300

    ED-1032 assets modificados para compatibilidade rota fallback 404

--- resources/views/components/errors/404.blade.php
+++ resources/views/components/errors/404.blade.php
@@ -1,37 +1,29 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	<!-- Apple devices fullscreen -->
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<!-- Apple devices fullscreen -->
-	<meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
 	<title>GECOM - Login</title>
 
 	<!-- Bootstrap -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 	<!-- Theme CSS -->
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 	<!-- Color CSS -->
-	<link rel="stylesheet" href="css/themes.css">
-
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/eakroko.js"></script>
+	<link rel="stylesheet" href="{{ asset('css/themes.css') }}">
 
 	<!-- Favicon -->
 	<link rel="shortcut icon" href="{{asset('img/favicon.ico')}}" />
 	<!-- Apple devices Homescreen icon -->
-	<link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-precomposed.png" />
+	<link rel="apple-touch-icon-precomposed" href="{{ asset('img/apple-touch-icon-precomposed.png') }}" />
 
 </head>
 
-<body class='404 theme-darkblue'>
+<body class='login theme-darkblue'>
 	<div class="wrapper">
 		<h1 style="color: #ffffff">Página não encontrada!<h1>
         <a href="{{ route('home') }}">
             <button class="btn btn-large">
-                <h4>VOLTAR AO INÍCIO</h4>
+                VOLTAR AO INICÍO
             </button>
         </a>
 	</div>
