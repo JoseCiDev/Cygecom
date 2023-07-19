6fa597d771261d98dc83603b82609478cb6ef277
Luis Pedro <luis.pedro@essentia.com.br>
2023-07-19 11:41:27 -0300

    ED-1044 Refatoração do seeder de fornecedores para usar a nova super classe SeederFromImporter abstraindo funcionalidades genéricas

--- database/seeders/PopulateSuppliers.php
+++ database/seeders/PopulateSuppliers.php
@@ -5,61 +5,50 @@ namespace Database\Seeders;
 use App\Models\Address;
 use App\Models\Phone;
 use App\Models\Supplier;
-use App\Providers\ValidatorService;
 use ErrorException;
 use Illuminate\Database\QueryException;
-use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
-class PopulateSuppliers extends Seeder
+class PopulateSuppliers extends SeederFromImporter
 {
-    public function __construct(private ValidatorService $validatorService)
-    {
-    }
-
     /**
      * @throws ErrorException
      */
-    public function run(): void
+    public function runImporter(): void
     {
-        $output = __DIR__ . "/PopulateSuppliers/data/suppliers.php";
-        $existSuppliersData = file_exists($output);
-
-        if ($existSuppliersData) {
-            $suppliersData = require($output);
-            DB::transaction(function () use ($suppliersData) {
-                try {
-                    foreach ($suppliersData as $data) {
-                        $validator = $this->validatorService->supplier($data);
-                        if ($validator->fails()) {
-                            continue;
-                        }
-
-                        if ((bool)$data['number']) {
-                            $phoneData = ['number' => $data['number'], 'phone_type' => 'commercial'];
-                            $phone = Phone::create($phoneData);
-                            $data['phone_id'] = $phone->id;
-                        }
-
-                        $address = new Address;
-                        $fillableData = array_intersect_key($data, array_flip($address->getFillable()));
-                        $address->fill($fillableData);
-                        $address->save();
-
-                        $data['address_id'] = $address->id;
-
-                        $supplier = new Supplier;
-                        $fillableData = array_intersect_key($data, array_flip($supplier->getFillable()));
-                        $supplier->fill($fillableData);
-                        $supplier->qualification = 'qualificado';
-                        $supplier->save();
+        $suppliersData = $this->data;
+        DB::transaction(function () use ($suppliersData) {
+            try {
+                foreach ($suppliersData as $data) {
+                    $validator = $this->validatorService->supplier($data);
+                    if ($validator->fails()) {
+                        continue;
                     }
-                } catch (QueryException $error) {
-                    DB::rollback();
-                    Log::error('Ocorreu um erro ao executar a transação: ' . $error->getMessage());
+
+                    if ((bool)$data['number']) {
+                        $phoneData = ['number' => $data['number'], 'phone_type' => 'commercial'];
+                        $phone = Phone::create($phoneData);
+                        $data['phone_id'] = $phone->id;
+                    }
+
+                    $address = new Address;
+                    $fillableData = array_intersect_key($data, array_flip($address->getFillable()));
+                    $address->fill($fillableData);
+                    $address->save();
+
+                    $data['address_id'] = $address->id;
+
+                    $supplier = new Supplier;
+                    $fillableData = array_intersect_key($data, array_flip($supplier->getFillable()));
+                    $supplier->fill($fillableData);
+                    $supplier->qualification = 'qualificado';
+                    $supplier->save();
                 }
-            });
-        }
+            } catch (QueryException $error) {
+                DB::rollback();
+                Log::error('Ocorreu um erro ao executar a transação: ' . $error->getMessage());
+            }
+        });
     }
 }
