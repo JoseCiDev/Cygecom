3b51116764c2122d0d81c28eb790f7b20f26a6dc
Luis Pedro <luis.pedro@essentia.com.br>
2023-07-24 16:26:35 -0300

    ED-1085 Melhoria no middleware de perfil do usuário

--- app/Http/Middleware/CheckProfileMiddleware.php
+++ app/Http/Middleware/CheckProfileMiddleware.php
@@ -11,11 +11,11 @@ class CheckProfileMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle($request, Closure $next, $profile)
+    public function handle($request, Closure $next, ...$profiles)
     {
         $isAdmin = $request->user()->profile->name === 'admin';
 
-        if ($isAdmin || $request->user()->profile->name === $profile) {
+        if ($isAdmin || in_array($request->user()->profile->name, $profiles)) {
             return $next($request);
         }
 
