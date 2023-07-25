8c025569c9954ad8463e83f94e7f529e8cbd217d
Luis Pedro <luis.pedro@essentia.com.br>
2023-07-25 18:53:08 -0300

    ED-1085 Ajustes de scripts js e melhorias de modal de analisar solicitação

--- public/js/supplies/modal-confirm-supplies-responsability.js
+++ public/js/supplies/modal-confirm-supplies-responsability.js
@@ -12,12 +12,12 @@ $(() => {
         }
 
         bootbox.confirm({
-            title: "Confirmar responsabilidade",
+            title: "Confirmar atribuição de solicitação",
             message: `Ainda não existe responsável por essa solicitação.
                     <br>
-                    Você aceita ficar como o responsável?
+                    Deseja se tornar o responsável?
                     <br>
-                    <small><i class="fa fa-warning"/> Aviso: Caso seja sua própria solicitação não é será possível ser o responsável.</small>`,
+                    <small>Aviso: Não será possível trocar o responsável posteriormente.</small>`,
             buttons: {
                 confirm: {
                     label: 'Concordar e ir',
