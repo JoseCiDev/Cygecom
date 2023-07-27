26913edb8e7e904ca67d197c6ac571ab9b72c699
Luis Pedro <luis.pedro@essentia.com.br>
2023-07-27 11:34:50 -0300

    ED-1092 Criação do EmailService com métodos para envio para usuário aprovador, status para solicitante e responsável para solicitante

---
+++ app/Providers/EmailService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Providers;
+
+use App\Mail\GenericEmail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\ServiceProvider;
+
+class EmailService extends ServiceProvider
+{
+    public function sendPendingApprovalEmail($purchaseRequest, $approver)
+    {
+        $subject = "Solicitação de " . $purchaseRequest->type->label() . " nº " . $purchaseRequest->id . ' - Aprovação pendente';
+        $message = "Olá, aprovador " . $purchaseRequest->user->approver->person->name . '! '
+            . "A solicitação de " . $purchaseRequest->type->label() . " nº " . $purchaseRequest->id . " está pendente."
+            . "Solicitante: " . $purchaseRequest->user->person->name . ". "
+            . "E-mail do solicitante: " . $purchaseRequest->user->email . ". "
+            . "Telefone/celular do solicitante: " . $purchaseRequest->user->person->phone->first()->number . ". "
+            . "Atribuição de responsável: " . $purchaseRequest->suppliesUser->person->name . '. '
+            . "E-mail do responsável: " . $purchaseRequest->suppliesUser->email . '. '
+            . "Telefone/celular do responsável: " . $purchaseRequest->suppliesUser->person->phone->first()->number . '. ';
+
+        $email = new GenericEmail($subject, $message);
+        Mail::to($approver->email)->send($email);
+    }
+
+    public function sendStatusUpdatedEmail($purchaseRequest)
+    {
+        $subject = "Solicitação de " . $purchaseRequest->type->label() . " nº " . $purchaseRequest->id . ' - Status atualizado para ' . $purchaseRequest->status->label();
+        $message = "Olá, " . $purchaseRequest->user->person->name . "! Sua solicitação de " . $purchaseRequest->type->label() . " teve o status atualizado para [" . $purchaseRequest->status->label() . '].';
+
+        $email = new GenericEmail($subject, $message);
+        Mail::to($purchaseRequest->user->email)->send($email);
+    }
+
+    public function sendResponsibleAssignedEmail($purchaseRequest)
+    {
+        $subject = "Solicitação de " . $purchaseRequest->type->label() . " nº " . $purchaseRequest->id . " - Atribuição de responsável";
+        $message = "Olá, " . $purchaseRequest->user->person->name . "! "
+            . "Sua solicitação de " . $purchaseRequest->type->label() . " nº " . $purchaseRequest->id . " foi atualizada. "
+            . "Foi atribuído um responsável pelo processo.";
+
+        $email = new GenericEmail($subject, $message);
+        Mail::to($purchaseRequest->user->email)->send($email);
+    }
+}
