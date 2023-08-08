d67d0a89e19394cfca5256c7fae47d4c75aa49ae
Cesar Sousa <cesar.luiz@essentia.com.br>
2023-08-08 10:38:01 -0300

    ED-1041 mascara telefone internacional adicionada cadastro fornecedores

--- resources/views/components/supplier/supplier-form.blade.php
+++ resources/views/components/supplier/supplier-form.blade.php
@@ -6,9 +6,9 @@
 @php
     $route = $isRegister ? route('supplier.register') : route('supplier.update', ['id' => $id]);
 @endphp
-<form method="POST" action="{{$route}}" class="form-validate" id="supplier-form" data-cy="supplier-form">
+<form method="POST" action="{{ $route }}" class="form-validate" id="supplier-form" data-cy="supplier-form">
     @csrf
-    
+
     <div class="row center-block" style="padding-bottom: 12px;">
         <div class="col-sm-6">
             <h4>DADOS FISCAIS</h4>
@@ -16,7 +16,9 @@
         <div class="col-sm-6">
             <div class="pull-right">
                 <fieldset> Consulte aqui o cartão CNPJ: </fieldset>
-                <a style="display: block" href="https://solucoes.receita.fazenda.gov.br/servicos/cnpjreva/cnpjreva_solicitacao.asp" target="_blank" rel="noopener noreferrer">Clique aqui para a pesquisa do cartão CNPJ</a>
+                <a style="display: block"
+                    href="https://solucoes.receita.fazenda.gov.br/servicos/cnpjreva/cnpjreva_solicitacao.asp"
+                    target="_blank" rel="noopener noreferrer">Clique aqui para a pesquisa do cartão CNPJ</a>
             </div>
         </div>
     </div>
@@ -33,9 +35,9 @@
         <div class="col-sm-5">
             <div class="form-group">
                 <label for="corporate_name" class="control-label">Razão social</label>
-                <input value="{{ $supplier?->corporate_name }}" type="text" name="corporate_name" id="corporate_name" data-cy="corporate_name"
-                    placeholder="Informe a razão social deste fornecedor" class="form-control" data-rule-required="true"
-                    data-rule-minlength="3">
+                <input value="{{ $supplier?->corporate_name }}" type="text" name="corporate_name" id="corporate_name"
+                    data-cy="corporate_name" placeholder="Informe a razão social deste fornecedor" class="form-control"
+                    data-rule-required="true" data-rule-minlength="3">
             </div>
         </div>
 
@@ -52,8 +54,8 @@
         <div class="col-sm-6">
             <div class="form-group">
                 <label for="description" class="control-label">Descrição</label>
-                <textarea name="description" id="description" data-cy="description" placeholder="Descreva a função da empresa" rows="3"
-                    class="form-control no-resize">{{ $supplier?->description }}</textarea>
+                <textarea name="description" id="description" data-cy="description" placeholder="Descreva a função da empresa"
+                    rows="3" class="form-control no-resize">{{ $supplier?->description }}</textarea>
             </div>
         </div>
         <div class="col-sm-3">
@@ -61,18 +63,18 @@
                 <label for="market-type" class="control-label">Tipo de mercado</label>
                 <fieldset id="market-type" data-rule-required="true">
                     <div class="row">
-                        <input @checked($supplier?->market_type === 'nacional') class="icheck-me" type="radio"
-                            name="market_type" id="nacional" data-cy="nacional" value="Nacional" data-skin="minimal" required>
+                        <input @checked($supplier?->market_type === 'nacional') class="icheck-me" type="radio" name="market_type"
+                            id="nacional" data-cy="nacional" value="Nacional" data-skin="minimal" required>
                         <label class="form-check-label" for="nacional">Mercado nacional</label>
                     </div>
                     <div class="row">
-                        <input @checked($supplier?->market_type === 'externo') class="icheck-me" type="radio"
-                            name="market_type" id="externo" data-cy="externo" value="Externo" data-skin="minimal" required>
+                        <input @checked($supplier?->market_type === 'externo') class="icheck-me" type="radio" name="market_type"
+                            id="externo" data-cy="externo" value="Externo" data-skin="minimal" required>
                         <label class="form-check-label" for="externo">Mercado externo</label>
                     </div>
                     <div class="row">
-                        <input @checked($supplier?->market_type === 'prospec') class="icheck-me" type="radio"
-                            name="market_type" id="prospec" data-cy="prospec" value="Prospecção" data-skin="minimal" required>
+                        <input @checked($supplier?->market_type === 'prospec') class="icheck-me" type="radio" name="market_type"
+                            id="prospec" data-cy="prospec" value="Prospecção" data-skin="minimal" required>
                         <label class="form-check-label" for="prospec">Prospecção</label>
                     </div>
                 </fieldset>
@@ -84,17 +86,20 @@
                 <fieldset id="supplier-indication" data-rule-required="true">
                     <div class="row">
                         <input @checked($supplier?->supplier_indication === 'M') class="icheck-me" type="radio"
-                            name="supplier_indication" id="materia-prima" data-cy="materia-prima" value="Matéria Prima" data-skin="minimal" required>
+                            name="supplier_indication" id="materia-prima" data-cy="materia-prima"
+                            value="Matéria Prima" data-skin="minimal" required>
                         <label class="form-check-label" for="materia-prima">Matéria-prima</label>
                     </div>
                     <div class="row">
                         <input @checked($supplier?->supplier_indication === 'S') class="icheck-me" type="radio"
-                            name="supplier_indication" id="servico" data-cy="servico" value="Serviço" data-skin="minimal" required>
+                            name="supplier_indication" id="servico" data-cy="servico" value="Serviço"
+                            data-skin="minimal" required>
                         <label class="form-check-label" for="servico">Serviço</label>
                     </div>
                     <div class="row">
                         <input @checked($supplier?->supplier_indication === 'A') class="icheck-me" type="radio"
-                            name="supplier_indication" id="ambos" value="Ambos" data-cy="ambos" data-skin="minimal" required>
+                            name="supplier_indication" id="ambos" value="Ambos" data-cy="ambos"
+                            data-skin="minimal" required>
                         <label class="form-check-label" for="ambos">Ambos</label>
                     </div>
                 </fieldset>
@@ -120,24 +125,24 @@
         <div class="col-sm-3">
             <div class="form-group">
                 <label for="country" class="control-label">País</label>
-                <input value="{{ $supplier?->address->country }}" type="text" name="country"
-                    id="country" data-cy="country" placeholder="País" class="form-control" data-rule-required="true">
+                <input value="{{ $supplier?->address->country }}" type="text" name="country" id="country"
+                    data-cy="country" placeholder="País" class="form-control" data-rule-required="true">
             </div>
         </div>
         {{-- ESTADO UF --}}
         <div class="col-sm-3">
             <div class="form-group">
                 <label for="state" class="control-label">Estado/UF</label>
-                <input value="{{ $supplier?->address->state }}" type="text" name="state" id="state" data-cy="state"
-                    placeholder="UF" class="form-control" data-rule-required="true">
+                <input value="{{ $supplier?->address->state }}" type="text" name="state" id="state"
+                    data-cy="state" placeholder="UF" class="form-control" data-rule-required="true">
             </div>
         </div>
         {{-- CIDADE --}}
         <div class="col-sm-3">
             <div class="form-group">
                 <label for="city" class="control-label">Cidade</label>
-                <input value="{{ $supplier?->address->city }}" type="text" name="city" id="city" data-cy="city"
-                    placeholder="Cidade" class="form-control" data-rule-required="true">
+                <input value="{{ $supplier?->address->city }}" type="text" name="city" id="city"
+                    data-cy="city" placeholder="Cidade" class="form-control" data-rule-required="true">
             </div>
         </div>
     </div>
@@ -147,15 +152,16 @@
             <div class="form-group">
                 <label for="neighborhood" class="control-label">Bairro</label>
                 <input value="{{ $supplier?->address->neighborhood }}" type="text" name="neighborhood"
-                    id="neighborhood" data-cy="neighborhood" placeholder="Bairro" class="form-control" data-rule-required="true">
+                    id="neighborhood" data-cy="neighborhood" placeholder="Bairro" class="form-control"
+                    data-rule-required="true">
             </div>
         </div>
         {{-- RUA --}}
         <div class="col-sm-4">
             <div class="form-group">
                 <label for="street" class="control-label">Rua</label>
-                <input value="{{ $supplier?->address->street }}" type="text" name="street"
-                    id="street" data-cy="street" placeholder="Rua/Avenida/Servidão" class="form-control"
+                <input value="{{ $supplier?->address->street }}" type="text" name="street" id="street"
+                    data-cy="street" placeholder="Rua/Avenida/Servidão" class="form-control"
                     data-rule-required="true">
             </div>
         </div>
@@ -163,13 +169,9 @@
         <div class="col-sm-2">
             <div class="form-group" style="margin-bottom:5px;">
                 <label for="street_number" class="control-label">Número</label>
-                <input
-                    value="{{ $supplier?->address->street_number }}"
-                    @readonly(collect($supplier)->isNotEmpty() && $supplier->address->street_number === null)
-                    type="text" name="street_number"
-                    id="street_number" data-cy="street_number" placeholder="Nº" class="form-control street-number"
-                    data-rule-required="true"
-                >
+                <input value="{{ $supplier?->address->street_number }}" @readonly(collect($supplier)->isNotEmpty() && $supplier->address->street_number === null) type="text"
+                    name="street_number" id="street_number" data-cy="street_number" placeholder="Nº"
+                    class="form-control street-number" data-rule-required="true">
                 {{-- checkbox sem numero --}}
                 <div class="no-limit"
                     style="
@@ -178,10 +180,8 @@
                     margin-top: 5px;
                     gap: 5px;
                 ">
-                    <input type="checkbox" id="checkbox-has-no-street-number" data-cy="checkbox-has-no-street-number" class="checkbox-has-no-street-number"
-                        style="margin:0"
-                        @checked(collect($supplier)->isNotEmpty() && $supplier->address->street_number === null)
-                    >
+                    <input type="checkbox" id="checkbox-has-no-street-number" data-cy="checkbox-has-no-street-number"
+                        class="checkbox-has-no-street-number" style="margin:0" @checked(collect($supplier)->isNotEmpty() && $supplier->address->street_number === null)>
                     <label for="checkbox-has-no-street-number" style="margin:0; font-size:13px">
                         Sem número.
                     </label>
@@ -207,17 +207,34 @@
         <div class="col-sm-3">
             <div class="form-group">
                 <label for="number" class="control-label">Telefone</label>
-                <input value="{{ $supplier?->phone?->number }}" type="text" name="number" id="number" data-cy="number"
-                    placeholder="(00) 0000-0000" class="form-control phone-number" data-rule-required="true"
-                    minLength="14">
+                <input value="{{ $supplier?->phone?->number }}" type="text" name="number" id="number"
+                    data-cy="number" placeholder="(00) 0000-0000" class="form-control phone-number"
+                    data-rule-required="true" minLength="10" maxLength="20">
                 <input type="hidden" name="phone_type" id="commercial" value="commercial" data-cy="commercial">
+
+                {{-- checkbox numero estrangeiro --}}
+                <div class="international-number"
+                    style="
+                        display: flex;
+                        align-items: center;
+                        margin-top: 5px;
+                        gap: 5px;
+                    ">
+                    <input type="checkbox" id="checkbox-international-number" data-cy="checkbox-international-number"
+                        value="{{ $supplier?->phone?->isInternational ? '1' : '0' }}" @checked($supplier?->phone?->isInternational)
+                        class="checkbox-international-number" style="margin:0" name="isInternational">
+                    <label for="checkbox-international-number" style="margin:0; font-size:13px">
+                        Número de telefone Internacional.
+                    </label>
+                </div>
+
             </div>
         </div>
         <div class="col-sm-4">
             <div class="form-group">
                 <label for="email" class="control-label">E-mail</label>
-                <input value="{{ $supplier?->email }}" type="email" name="email" id="email" data-cy="email"
-                    placeholder="user_email@fornecedor.com.br" class="form-control" data-rule-email="true"
+                <input value="{{ $supplier?->email }}" type="email" name="email" id="email"
+                    data-cy="email" placeholder="user_email@fornecedor.com.br" class="form-control" data-rule-email="true"
                     data-rule-required="true">
             </div>
         </div>
@@ -264,21 +281,57 @@
             $streetNumber.prop('readonly', isChecked).val(currentValue).valid();
         });
 
-        // masks
         $postalCode.imask({
             mask: '00.000-000'
         });
+
         $streetNumber.imask({
             mask: Number,
         });
-        $phoneNumber.imask({
-            mask: [{
-                    mask: '(00) 0000-0000'
-                },
-                {
-                    mask: '(00) 00000-0000'
+
+
+        // checkbox número internacional (mudança mascara)
+        const $checkboxInternationalNumber = $('#checkbox-international-number');
+        const checkboxInternationalNumberValue = $checkboxInternationalNumber.val();
+
+        let phoneMask;
+
+        if (checkboxInternationalNumberValue === "0") {
+            phoneMask = $phoneNumber.imask({
+                mask: [{
+                        mask: '(00) 0000-0000'
+                    },
+                    {
+                        mask: '(00) 00000-0000'
+                    }
+                ]
+            });
+        }
+
+        $checkboxInternationalNumber.on('click', function() {
+            const isChecked = $(this).is(':checked');
+            $phoneNumber.valid();
+
+            const valueToSend = isChecked ? "1" : "0";
+            $(this).val(valueToSend);
+
+            if (isChecked) {
+                if ($phoneNumber.val()) {
+                    $(this).closest('.form-group').removeClass('has-error');
+                    $(this).closest('.form-group').removeClass('has-success');
                 }
-            ]
-        });
+                phoneMask.destroy();
+            } else {
+                phoneMask = $phoneNumber.imask({
+                    mask: [{
+                            mask: '(00) 0000-0000'
+                        },
+                        {
+                            mask: '(00) 00000-0000'
+                        }
+                    ]
+                });
+            }
+        })
     });
 </script>
