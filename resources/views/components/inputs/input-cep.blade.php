abb7cce4a55f96627e84fa5e2b445deef02808ed
Luis Pedro <luis.pedro@essentia.com.br>
2023-08-09 20:03:21 -0300

    ED-1128 Habilitado consulta de endereço por CEP para atualizações de fornecedores

--- resources/views/components/inputs/input-cep.blade.php
+++ resources/views/components/inputs/input-cep.blade.php
@@ -6,43 +6,41 @@
     $(() => $('#{{$id}}').imask({ mask: '00.000-000' }));
 </script>
 
-@if (!$value)
-    <script>
-        $(() => {
-            const $postalCode = $('#{{$id}}');
-            $postalCode.on('input', function() {
-                if ($(this).val().length < 10) {
-                    return;
+<script>
+    $(() => {
+        const $postalCode = $('#{{$id}}');
+        $postalCode.on('input', function() {
+            if ($(this).val().length < 10) {
+                return;
+            }
+            const $inputFields = $('#{{$id}}, #country, #state, #city, #neighborhood, #street, #complement');
+            const $loadingIcon = $('.loading-icon');
+
+            $inputFields.prop('disabled', true);
+            $loadingIcon.show();
+
+            const postalCode = $postalCode.val().replace(/\D/g, '');
+                if (postalCode.length === 8) {
+                    const apiUrl = `https://viacep.com.br/ws/${postalCode}/json`;
+
+                    $.getJSON(apiUrl, function(data) {
+                        if (data.erro) {
+                            bootbox.alert("Endereço não encontrado para esse CEP.");
+                            return;
+                        }
+
+                        $('#country').val('Brasil');
+                        $('#state').val(data.uf);
+                        $('#city').val(data.localidade);
+                        $('#neighborhood').val(data.bairro);
+                        $('#street').val(data.logradouro);
+                        $('#complement').val(data.complemento);
+                    }).done(() => {
+                        $inputFields.prop('disabled', false);
+                        $loadingIcon.hide();
+                        $postalCode.valid();
+                    })
                 }
-                const $inputFields = $('#{{$id}}, #country, #state, #city, #neighborhood, #street, #complement');
-                const $loadingIcon = $('.loading-icon');
-
-                $inputFields.prop('disabled', true);
-                $loadingIcon.show();
-
-                const postalCode = $postalCode.val().replace(/\D/g, '');
-                    if (postalCode.length === 8) {
-                        const apiUrl = `https://viacep.com.br/ws/${postalCode}/json`;
-
-                        $.getJSON(apiUrl, function(data) {
-                            if (data.erro) {
-                                bootbox.alert("Endereço não encontrado para esse CEP.");
-                                return;
-                            }
-
-                            $('#country').val('Brasil');
-                            $('#state').val(data.uf);
-                            $('#city').val(data.localidade);
-                            $('#neighborhood').val(data.bairro);
-                            $('#street').val(data.logradouro);
-                            $('#complement').val(data.complemento);
-                        }).done(() => {
-                            $inputFields.prop('disabled', false);
-                            $loadingIcon.hide();
-                            $postalCode.valid();
-                        })
-                    }
-            })
-        });
-    </script>
-@endif
+        })
+    });
+</script>
