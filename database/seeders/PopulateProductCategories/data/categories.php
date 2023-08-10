dfe12281ed37ef2dfec78d9453d7d01e174d2434
Luis Pedro <luis.pedro@essentia.com.br>
2023-08-10 10:18:47 -0300

    ED-1132 Criação de arquivos que retornam arrays necessários para os seeders

---
+++ database/seeders/PopulateProductCategories/data/categories.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    [
+        'name' => 'Material Utilizado na Produção/Lab',
+        'description' => null
+    ],
+    [
+        'name' => 'Compra Laboratório Liquido, Sólido e Inj',
+        'description' => null
+    ],
+    [
+        'name' => 'Material de Limpeza e Copa',
+        'description' => null
+    ],
+    [
+        'name' => 'Revenda / Bonificado',
+        'description' => 'Mercadoria que vai ser revendida e também distribuida gratuitamente para os nossos clientes (Bonificados). Ex: Viseira, Garrafa, etc...'
+    ],
+    [
+        'name' => 'Brinde',
+        'description' => 'Mercadoria distribuida gratuitamente para nossos clientes e que não podemos vender. Ex. Toalha, Necessaire, etc...'
+    ],
+    [
+        'name' => 'Material Promocional',
+        'description' => 'Material publicitário/gráfico que visa promover a propaganda e melhor visualização dos nossos produtos. Ex: Lâminas, Folders, etc...'
+    ],
+    [
+        'name' => 'Amostra Grátis',
+        'description' => 'Solicitada para o fornecedor'
+    ],
+    [
+        'name' => 'Material de escritório',
+        'description' => null
+    ],
+    [
+        'name' => 'Outros Tipos de Produto',
+        'description' => null
+    ],
+];
