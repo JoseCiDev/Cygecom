ca8311e8888867be7e994e00a1b9d4758f920cd9
Luis Pedro <luis.pedro@essentia.com.br>
2023-08-17 16:32:26 -0300

    ED-1150 Adição de componente RequestFiles e validações nos formulários de solicitação

--- resources/views/components/purchase-request/purchase-request-form-service.blade.php
+++ resources/views/components/purchase-request/purchase-request-form-service.blade.php
@@ -459,40 +459,7 @@
             {{-- ARQUIVOS --}}
             <div class="row justify-content-center">
                 <div class="col-sm-12">
-                    <fieldset id="files-group">
-                        <h4 style="margin-bottom: 20px;">
-                            <i class="fa fa-paperclip"></i> Anexos
-                        </h4>
-                        <input type="file" class="form-control" name="arquivos[]" data-cy="arquivos" multiple>
-                        <ul class="list-group" style="margin-top:15px">
-                            @if (isset($files))
-                                @foreach ($files as $each)
-                                    @php
-                                        $filenameSearch = explode('/', $each->original_name);
-                                        $filename = end($filenameSearch);
-                                    @endphp
-                                    <li class="list-group-item" data-id-purchase-request-file="{{ $each->id }}">
-                                        <div class="row">
-                                            <div class="col-xs-6">
-                                                <i class='fa fa-file'></i><a style='margin-left:5px'
-                                                    href="{{ env('AWS_S3_BASE_URL') . $each->path }}"target="_blank">{{ $filename }}</a>
-                                            </div>
-                                            <div class="col-xs-6 text-right">
-                                                <button type="button" class="btn btn-primary file-remove"><i
-                                                        class='fa fa-trash'
-                                                        style='margin-right:5px'></i>Excluir</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                @endforeach
-                            @endif
-                        </ul>
-                        <div class="alert alert-success" style="display:none;margin:15px 0px 15px 0px;"><i
-                                class="fa fa-check"></i> Excluido com sucesso!</div>
-                        <div class="alert alert-danger" style="display:none;margin:15px 0px 15px 0px;"><i
-                                class="fa fa-close"></i> Não foi possível excluir, por favor tente novamente mais
-                            tarde.</div>
-                    </fieldset>
+                    <x-RequestFiles :purchaseRequestId="$purchaseRequest?->id" :isCopy="$isCopy" />
                 </div>
             </div>
 
@@ -531,7 +498,6 @@
 
 <script src="{{ asset('js/supplies/select2-custom.js') }}"></script>
 <script src="{{ asset('js/service-form/desired-date-config.js') }}"></script>
-<script src="{{ asset('js/service-form/file-remove-button-config.js') }}"></script>
 <script src="{{ asset('js/service-form/submit-buttons-config.js') }}"></script>
 <script src="{{ asset('js/service-form/imasks.js') }}"></script>
 
