bbd3c8bab6b935fe7e02f90b3b970c7eddd74f85
Luis Pedro <luis.pedro@essentia.com.br>
2023-08-24 19:28:00 -0300

    ED-1171 Correção de bug ao não conseguir enviar request. Checkbox de already_provided required fica checked usando prop

--- resources/views/components/purchase-request/purchase-request-form-service.blade.php
+++ resources/views/components/purchase-request/purchase-request-form-service.blade.php
@@ -418,8 +418,8 @@
                             style="width:100%;">
                             <option value=""></option>
                             @foreach ($suppliers as $supplier)
-                                @php 
-                                    $supplierSelected = isset($purchaseRequest->service) && $purchaseRequest->service->supplier_id === $supplier->id; 
+                                @php
+                                    $supplierSelected = isset($purchaseRequest->service) && $purchaseRequest->service->supplier_id === $supplier->id;
                                     $cnpj = $supplier->cpf_cnpj ? preg_replace('/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/', '$1.$2.$3/$4-$5', $supplier->cpf_cnpj) : 'CNPJ indefinido'
                                 @endphp
                                 <option value="{{ $supplier->id }}" @selected($supplierSelected)>{{ "$cnpj - $supplier->corporate_name" }}</option>
@@ -803,7 +803,8 @@
             if (isContractedBySupplies) {
                 $serviceAlreadyProvided
                     .last()
-                    .attr('checked', true);
+                    .prop('checked', true)
+                    .valid();
 
                 $divAlreadyProvided
                     .attr('hidden', true);
@@ -838,7 +839,7 @@
                 if (!purchaseRequest) {
                     $serviceAlreadyProvided
                         .last()
-                        .attr('checked', false);
+                        .prop('checked', false);
                 }
 
                 $divAlreadyProvided
