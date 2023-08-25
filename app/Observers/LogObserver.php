5a39976fccff3f8f672adad5d3c4049ef08ff25f
Luis Pedro <luis.pedro@essentia.com.br>
2023-08-25 15:34:42 -0300

    ED-1160 Criação de trait para usar em Models que devem ter logs atraveś do LogObserver

---
+++ app/Observers/LogObserver.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Observers;
+
+use App\Enums\LogAction;
+use App\Models\Log;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class LogObserver
+{
+    /**
+     * Handle the Contract "created" event.
+     */
+    public function created(Model $model): void
+    {
+        $changes = $model->getAttributes();
+
+        $this->createLog(LogAction::CREATE, $model, $changes);
+    }
+
+    /**
+     * Handle the Contract "updated" event.
+     */
+    public function updated(Model $model): void
+    {
+        $dirtyAttributes = $model->getDirty();
+
+        if (!empty($dirtyAttributes)) {
+            $isDelete = $model->wasChanged('deleted_at') && $model->deleted_at !== null;
+
+            $action = $isDelete ? 'delete' : 'update';
+            $action = LogAction::tryFrom($action);
+            $this->createLog($action, $model, $dirtyAttributes);
+        }
+    }
+
+    private function createLog(LogAction $action, $model, ?array $changes = null)
+    {
+        Log::create([
+            'table' => $model->getTable(),
+            'foreign_id' => $model->id,
+            'user_id' => Auth::id(),
+            'action' => $action->value,
+            'changes' => $changes,
+        ]);
+    }
+}
