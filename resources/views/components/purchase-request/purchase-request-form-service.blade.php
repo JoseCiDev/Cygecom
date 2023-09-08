18734e7a97b0123400ea02a09efd054f6ad472a6
Cesar Sousa <cesar.luiz@essentia.com.br>
2023-09-08 15:56:16 -0300

    altera attr para prop em checked

--- resources/views/components/purchase-request/purchase-request-form-service.blade.php
+++ resources/views/components/purchase-request/purchase-request-form-service.blade.php
@@ -196,7 +196,7 @@
                 </div>
 
                 {{-- SERVIÇO JÁ PRESTADO --}}
-                <div class="col-sm-2 div-already-provided" hidden>
+                <div class="col-sm-2 div-already-provided">
                     <div class="form-group">
                         <label for="form-check" class="regular-text" style="padding-right:10px;">
                             Este serviço já foi prestado?
@@ -774,7 +774,7 @@
             if (isContractedBySupplies) {
                 $serviceAlreadyProvided
                     .last()
-                    .attr('checked', true);
+                    .prop('checked', true);
 
                 $divAlreadyProvided
                     .attr('hidden', true);
@@ -809,7 +809,7 @@
                 if (!purchaseRequest) {
                     $serviceAlreadyProvided
                         .last()
-                        .attr('checked', false);
+                        .prop('checked', false);
                 }
 
                 $divAlreadyProvided
