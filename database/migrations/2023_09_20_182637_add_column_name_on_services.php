902bb08c953183284210a93fc12fbf56bc24caa5
Cesar Sousa <cesar.luiz@essentia.com.br>
2023-09-20 15:47:39 -0300

    ED-1228 WIP: coluna name inserida na tabela services

---
+++ database/migrations/2023_09_20_182637_add_column_name_on_services.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->string('name', 255)->unique();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->dropColumn('name');
+        });
+    }
+};
