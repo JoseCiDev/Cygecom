ff29a04fb62bbaaa6bda813ed6d03d204f16be88
Cesar Sousa <cesar.luiz@essentia.com.br>
2023-09-28 10:31:00 -0300

    ED-1241 adiciona coluna can_associate_requester e backend

---
+++ database/migrations/2023_09_28_120843_add_can_associate_requester_to_users.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('can_associate_requester')->default(false);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('can_associate_requester');
+        });
+    }
+};
