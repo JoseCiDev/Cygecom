7d9e3abdffe0063a9e09ab5f3d2429e3a04a5610
Cesar Sousa <cesar.luiz@essentia.com.br>
2023-10-03 08:00:49 -0300

    ED-1206 rotas de detalhes solicitacoes fornecedores

--- routes/web/suppliers.php
+++ routes/web/suppliers.php
@@ -10,5 +10,10 @@ Route::middleware(['auth', 'profile:gestor_fornecedores'])->group(function () {
 
         Route::post('/delete/{id}', [App\Http\Controllers\SupplierController::class, 'delete']);
         Route::post('/update/{id}', [App\Http\Controllers\SupplierController::class, 'update'])->name('supplier.update');
+
+        // acessar detalhes de suprimentos
+        Route::get('/service/{id}/details', [App\Http\Controllers\ServiceController::class, 'details'])->name('supplier.service.detail');
+        Route::get('/product/{id}/details', [App\Http\Controllers\ProductController::class, 'details'])->name('supplier.product.detail');
+        Route::get('/contract/{id}/details', [App\Http\Controllers\ContractController::class, 'details'])->name('supplier.contract.detail');
     });
 });
