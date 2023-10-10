c7d004ab956e1ae7417773bd05990d9228812b67
Cesar Sousa <cesar.luiz@essentia.com.br>
2023-10-10 14:23:55 -0300

    ED-1211 adiciona softDeletes no model de categoria de produto

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProductCategory extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function productSugestion()
     {
