c5bd8be4c41fda1ae1ce40cc238d14f965375a1e
Luis Pedro <luis.pedro@essentia.com.br>
2023-10-10 11:30:06 -0300

    ED-1298 Limpeza de importações desnecessárias no blade de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,18 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	<!-- Apple devices fullscreen -->
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<!-- Apple devices fullscreen -->
-	<meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
 	<title>GECOM - Login</title>
 
+    <!-- Favicon -->
+	<link rel="shortcut icon" href="{{asset('img/gecom/favicon.png')}}" />
+
 	<!-- Bootstrap -->
 	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<!-- icheck -->
-	<link rel="stylesheet" href="css/plugins/icheck/all.css">
 	<!-- Theme CSS -->
 	<link rel="stylesheet" href="css/style.css">
-	<!-- Color CSS -->
-	<link rel="stylesheet" href="css/themes.css">
-
-	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
-
-	<!-- Nice Scroll -->
-	<script src="js/plugins/nicescroll/jquery.nicescroll.min.js"></script>
-	<!-- Validation -->
-	<script src="js/plugins/validation/jquery.validate.min.js"></script>
-	<script src="js/plugins/validation/additional-methods.min.js"></script>
-	<!-- icheck -->
-	<script src="js/plugins/icheck/jquery.icheck.min.js"></script>
 	<!-- Bootstrap -->
 	<script src="js/bootstrap.min.js"></script>
-	<script src="js/eakroko.js"></script>
-
-	<!--[if lte IE 9]>
-		<script src="js/plugins/placeholder/jquery.placeholder.min.js"></script>
-		<script>
-			$(document).ready(function() {
-				$('input, textarea').placeholder();
-			});
-		</script>
-	<![endif]-->
-
-	<!-- Favicon -->
-	<link rel="shortcut icon" href="{{asset('img/gecom/favicon.png')}}" />
-	<!-- Apple devices Homescreen icon -->
-	{{-- <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-precomposed.png" /> --}}
-
 </head>
 
 <style>
