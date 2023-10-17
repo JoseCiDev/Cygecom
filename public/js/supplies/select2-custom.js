a1abe8548630188d2abb3e2cb1a93ef869fc01ed
Luis Pedro <luis.pedro@essentia.com.br>
2023-10-17 17:39:54 -0300

    ED-1298 Melhorias no select2-custom.js

--- public/js/supplies/select2-custom.js
+++ public/js/supplies/select2-custom.js
@@ -1,31 +1,23 @@
-function openSupplierModalAndFocusInput() {
-    $("#modal-supplier-register").modal("show");
-    $("#select2-drop").hide();
-}
-
-// adiciona botão de cadastro de fornecedor em selects dinamicos (chamar no evento)
-function addBtnSupplierSelect(element) {
-    $(element).select2({
-        formatNoMatches: function (term) {
-            return `
-            <button
-                type="button"
-                rel="tooltip"
-                title="Registrar novo fornecedor"
-                class="btn btn-secondary btn-small"
-                onclick="openSupplierModalAndFocusInput();"
-            >
-                <i class="fa fa-plus"></i> Registar novo fornecedor
-            </button>
-        `;
-        },
-    });
-}
-
 $(() => {
-    const elementsWithSupplierID = document.querySelectorAll(
-        'select[name*="supplier_id"]'
-    );
+    const $buttonAddSupplier = $(`
+        <button
+            rel="tooltip"
+            title="Registrar novo fornecedor"
+            class="btn btn-secondary btn-small add-btn-supplier"
+            onclick="openSupplierModalAndFocusInput(event)"
+        >
+            <i class="fa fa-plus"></i> Registar novo fornecedor
+        </button>
+    `);
 
-    addBtnSupplierSelect(elementsWithSupplierID);
+    window.openSupplierModalAndFocusInput = function() {
+        $("#modal-supplier-register").modal("show");
+        $("#select2-drop").hide();
+    }
+
+    window.addBtnSupplierSelect = (element) => $(element).select2({ formatNoMatches: () => $buttonAddSupplier[0].outerHTML });
+
+    const elementsWithSupplierID = $('select[name*="supplier_id"]');
+    elementsWithSupplierID.select2( { formatNoMatches: () => $buttonAddSupplier[0].outerHTML } );
 });
+
