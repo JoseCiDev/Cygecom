22f4421184b27b4087ed0773eba7243bd729586e
Luis Pedro <luis.pedro@essentia.com.br>
2023-10-26 10:53:24 -0300

    ED-1347 Configurações de vite e upgrade de versões no package.json e composer.json

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,6 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
 
-                'node_modules/bootstrap/dist/js/bootstrap.min.js', // Bootstrap v3.2.0 - incompatível com ES6
-                'node_modules/select2/select2.js', // select2 v3.5.1 - incompatível com ES6
-
                 'public/js/plugins/validation/jquery.validate.min.js', // v1.13.1
                 'public/js/plugins/validation/additional-methods.min.js' // v1.13.1,
             ]
