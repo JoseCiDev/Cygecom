7552f76c46b803e0f5c50056185b3167740fda39
Luis Pedro <luis.pedro@essentia.com.br>
2023-11-27 15:17:16 -0300

    ED-1330 Criação de AbilitiesController com método index e storeJson

---
+++ app/Http/Controllers/AbilitiesController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\User\StoreAbilitiesRequest;
+use App\Models\{UserProfile, User, Ability};
+use App\Providers\UserService;
+
+class AbilitiesController extends Controller
+{
+    public function __construct(private UserService $userService)
+    {
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $abilities = Ability::with('users', 'profiles')->get();
+        $profiles = UserProfile::with('abilities', 'user.person')->get();
+        $users = User::with('abilities', 'person')->whereNull('deleted_at')->get();
+
+        $params = [
+            'abilities' => $abilities,
+            'profiles' => $profiles,
+            'users' => $users
+        ];
+
+        return view('abilities', $params);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreAbilitiesRequest $request
+     */
+    public function storeJson(StoreAbilitiesRequest $request, int $userId)
+    {
+        $validated = $request->validated();
+        $abilities = $validated['abilities'] ?? [];
+
+        $user = $this->userService->getUserById($userId);
+        $user->abilities()->sync($abilities);
+
+        return response()->json($user, 200);
+    }
+}
