c084019cd2afa56f16535a9d0ca5d518ab499105
Luis Pedro <luis.pedro@essentia.com.br>
2023-11-27 14:38:35 -0300

    ED-1330 Criação das tabelas abilities, abilities_profiles e abilities_users

---
+++ database/migrations/2023_11_07_213550_create_abilities_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\{User, UserProfile, Ability};
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('abilities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('description')->nullable();
+        });
+
+        Schema::create('abilities_profiles', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(UserProfile::class)->constrained('user_profiles');
+            $table->foreignIdFor(Ability::class)->constrained('abilities');
+
+            $table->unique(['user_profile_id', 'ability_id']);
+        });
+
+        Schema::create('abilities_users', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(User::class)->constrained('users');
+            $table->foreignIdFor(Ability::class)->constrained('abilities');
+
+            $table->unique(['user_id', 'ability_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('abilities_profiles');
+        Schema::dropIfExists('abilities_users');
+        Schema::dropIfExists('abilities');
+    }
+};
