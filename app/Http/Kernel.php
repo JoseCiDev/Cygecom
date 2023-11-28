14bbdb161cb687d7c9116d7bd04d45288ada9e1f
Luis Pedro <luis.pedro@essentia.com.br>
2023-11-28 18:10:55 -0300

    ED-1330 Exclusão do CheckProfileMiddleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,6 @@ class Kernel extends HttpKernel
         'signed'           => \App\Http\Middleware\ValidateSignature::class,
         'throttle'         => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified'         => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'profile' => \App\Http\Middleware\CheckProfileMiddleware::class,
         'supplies.group' => \App\Http\Middleware\CheckSuppliesGroupMiddleware::class,
     ];
 }
