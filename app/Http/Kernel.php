0db154e24647fffe402c0a4b9708353e6448aac2
Luis Pedro <luis.pedro@essentia.com.br>
2023-11-29 13:37:10 -0300

    ED-1330 Exclusão do CheckSuppliesGroupMiddleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,5 @@ class Kernel extends HttpKernel
         'signed'           => \App\Http\Middleware\ValidateSignature::class,
         'throttle'         => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified'         => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'supplies.group' => \App\Http\Middleware\CheckSuppliesGroupMiddleware::class,
     ];
 }
