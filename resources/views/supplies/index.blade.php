04c030e7a10797daa584324880b90da8e6b134fb
Cesar Sousa <cesar.luiz@essentia.com.br>
2024-01-22 14:42:41 -0300

    ED-1330 corrige conflitos dashboard suprimentos

--- resources/views/supplies/index.blade.php
+++ resources/views/supplies/index.blade.php
@@ -1,7 +1,6 @@
 <x-app>
-
     <div class="supplies-dashboard">
-        <h1 class="supplies-dashboard-title page-title">Dashboard de suprimentos</h1>
+        <h1 class="supplies-dashboard-title page-title">Dashboard de suprimentos | Solicitações pendentes</h1>
 
         <div class="supplies-dashboard-requests">
             <div class="supplies-dashboard-requests-item border-product">
@@ -9,8 +8,9 @@
                 <div class="supplies-dashboard-requests-item-info">
                     <div class="supplies-dashboard-requests-item-info-top">
                         <p class="supplies-dashboard-requests-item-info-top-price">
-                            <span class="supplies-dashboard-requests-item-info-top-price-value text-product-color">{{ $productQtd }}</span>
-                            solicitações no total
+                            <span
+                                class="supplies-dashboard-requests-item-info-top-price-value text-product-color">{{ $productsTotal }}</span>
+                            solicitações pendentes no total
                         </p>
                         <div class="supplies-dashboard-requests-item-info-top-btns">
                             @can('get.supplies.product.index')
@@ -21,51 +21,54 @@
                         </div>
                     </div>
                     <div class="supplies-dashboard-requests-item-info-bottom">
-                        @can('admin')
+
+                        <div class="supplies-dashboard-requests-item-info-comex-desired-date">
                             <div class="supplies-dashboard-requests-item-info-bottom-row">
                                 <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                    {{ $productsFromInp->count() }}
+                                    {{ $productDesiredTodayQtd }}
                                 </span>
                                 <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                    solicitações para INP/Noorskin/Oasis
+                                    desejadas para hoje
                                 </p>
                             </div>
                             <div class="supplies-dashboard-requests-item-info-bottom-row">
                                 <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                    {{ $productsFromHkm->count() }}
+                                    {{ $productComexQtd }}
                                 </span>
                                 <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                    solicitações para farmácias e demais empresas
+                                    COMEX
                                 </p>
                             </div>
-                        @endcan
-                        <div class="supplies-dashboard-requests-item-info-bottom-row">
-                            <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                {{ $productComexQtd }}
-                            </span>
-                            <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                COMEX
-                            </p>
                         </div>
-                        <div class="supplies-dashboard-requests-item-info-bottom-row">
-                            <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                {{ $productDesiredTodayQtd }}
-                            </span>
-                            <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                desejadas para hoje
-                            </p>
-                        </div>
+
+                        @foreach ($productsQtdByCompany as $company => $qtd)
+                            @php
+                                if ($qtd === 0) {
+                                    continue;
+                                }
+                            @endphp
+                            <div class="supplies-dashboard-requests-item-info-bottom-row">
+                                <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
+                                    {{ $qtd }}
+                                </span>
+                                <p class="supplies-dashboard-requests-item-info-bottom-row-text">
+                                    {{ $company }}
+                                </p>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
 
             <div class="supplies-dashboard-requests-item border-service">
-                <h2 class="supplies-dashboard-requests-item-title bg-service-color">Solicitações de serviços pontuais</h2>
+                <h2 class="supplies-dashboard-requests-item-title bg-service-color">Solicitações de serviços pontuais
+                </h2>
                 <div class="supplies-dashboard-requests-item-info">
                     <div class="supplies-dashboard-requests-item-info-top">
                         <p class="supplies-dashboard-requests-item-info-top-price">
-                            <span class="supplies-dashboard-requests-item-info-top-price-value text-service-color">{{ $serviceQtd }}</span>
-                            solicitações no total
+                            <span
+                                class="supplies-dashboard-requests-item-info-top-price-value text-service-color">{{ $servicesTotal }}</span>
+                            solicitações pendentes no total
                         </p>
                         <div class="supplies-dashboard-requests-item-info-top-btns">
                             @can('get.supplies.service.index')
@@ -76,51 +79,52 @@
                         </div>
                     </div>
                     <div class="supplies-dashboard-requests-item-info-bottom">
-                        @can('admin')
+                        <div class="supplies-dashboard-requests-item-info-comex-desired-date">
                             <div class="supplies-dashboard-requests-item-info-bottom-row">
                                 <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                    {{ $servicesFromInp->count() }}
+                                    {{ $serviceDesiredTodayQtd }}
                                 </span>
                                 <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                    solicitações para INP/Noorskin/Oasis
+                                    desejadas para hoje
                                 </p>
                             </div>
                             <div class="supplies-dashboard-requests-item-info-bottom-row">
                                 <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                    {{ $servicesFromHkm->count() }}
+                                    {{ $serviceComexQtd }}
                                 </span>
                                 <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                    solicitações para farmácias e demais empresas
+                                    COMEX
                                 </p>
                             </div>
-                        @endcan
-                        <div class="supplies-dashboard-requests-item-info-bottom-row">
-                            <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                {{ $serviceComexQtd }}
-                            </span>
-                            <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                COMEX
-                            </p>
                         </div>
-                        <div class="supplies-dashboard-requests-item-info-bottom-row">
-                            <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                {{ $serviceDesiredTodayQtd }}
-                            </span>
-                            <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                desejadas para hoje
-                            </p>
-                        </div>
+                        @foreach ($servicesQtdByCompany as $company => $qtd)
+                            @php
+                                if ($qtd === 0) {
+                                    continue;
+                                }
+                            @endphp
+                            <div class="supplies-dashboard-requests-item-info-bottom-row">
+                                <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
+                                    {{ $qtd }}
+                                </span>
+                                <p class="supplies-dashboard-requests-item-info-bottom-row-text">
+                                    {{ $company }}
+                                </p>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
 
             <div class="supplies-dashboard-requests-item border-contract">
-                <h2 class="supplies-dashboard-requests-item-title bg-contract-color">Solicitações de serviços recorrentes</h2>
+                <h2 class="supplies-dashboard-requests-item-title bg-contract-color">Solicitações de serviços
+                    recorrentes</h2>
                 <div class="supplies-dashboard-requests-item-info">
                     <div class="supplies-dashboard-requests-item-info-top">
                         <p class="supplies-dashboard-requests-item-info-top-price">
-                            <span class="supplies-dashboard-requests-item-info-top-price-value text-contract-color">{{ $contractQtd }}</span>
-                            solicitações no total
+                            <span
+                                class="supplies-dashboard-requests-item-info-top-price-value text-contract-color">{{ $contractsTotal }}</span>
+                            solicitações pendentes no total
                         </p>
                         <div class="supplies-dashboard-requests-item-info-top-btns">
                             @can('get.supplies.contract.index')
@@ -131,44 +135,42 @@
                         </div>
                     </div>
                     <div class="supplies-dashboard-requests-item-info-bottom">
-                        @can('admin')
+                        <div class="supplies-dashboard-requests-item-info-comex-desired-date">
                             <div class="supplies-dashboard-requests-item-info-bottom-row">
                                 <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                    {{ $contractsFromInp->count() }}
+                                    {{ $contractDesiredTodayQtd }}
                                 </span>
                                 <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                    solicitações para INP/Noorskin/Oasis
+                                    desejadas para hoje
                                 </p>
                             </div>
                             <div class="supplies-dashboard-requests-item-info-bottom-row">
                                 <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                    {{ $contractsFromHkm->count() }}
+                                    {{ $contractComexQtd }}
                                 </span>
                                 <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                    solicitações para farmácias e demais empresas
+                                    COMEX
                                 </p>
                             </div>
-                        @endcan
-                        <div class="supplies-dashboard-requests-item-info-bottom-row">
-                            <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                {{ $contractComexQtd }}
-                            </span>
-                            <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                COMEX
-                            </p>
                         </div>
-                        <div class="supplies-dashboard-requests-item-info-bottom-row">
-                            <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
-                                {{ $contractDesiredTodayQtd }}
-                            </span>
-                            <p class="supplies-dashboard-requests-item-info-bottom-row-text">
-                                desejadas para hoje
-                            </p>
-                        </div>
+                        @foreach ($contractQtdByCompany as $company => $qtd)
+                            @php
+                                if ($qtd === 0) {
+                                    continue;
+                                }
+                            @endphp
+                            <div class="supplies-dashboard-requests-item-info-bottom-row">
+                                <span class="supplies-dashboard-requests-item-info-bottom-row-text-qtd">
+                                    {{ $qtd }}
+                                </span>
+                                <p class="supplies-dashboard-requests-item-info-bottom-row-text">
+                                    {{ $company }}
+                                </p>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 </x-app>
