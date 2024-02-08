033f10da777ba9a3632292a43dc3f5ca73dfe25a
Luis Pedro <luis.pedro@essentia.com.br>
2024-02-08 11:49:51 -0300

    ED-1516 Refatoração e melhorias do script colvis de mostrar/esconder colunas

--- resources/js/app.js
+++ resources/js/app.js
@@ -41,6 +41,10 @@ window.createChartBar = createChartBar
 import Enum from './enums/enum.js';
 window.Enum = Enum;
 
+$.fn.getUserIdLogged = () => $('meta[name="user-id"]').first().attr('content');
+$.fn.getStorageItem = (itemName) => JSON.parse(localStorage.getItem(itemName));
+$.fn.saveOnStorage = (itemName, object) => localStorage.setItem(itemName, JSON.stringify(object));
+
 $.fn.imask = function(options) {
     const maskedElements = this.map((_, input) => new IMask(input, options)).toArray();
 
