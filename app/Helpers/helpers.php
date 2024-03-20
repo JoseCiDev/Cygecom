ba8abd383585db31ef4f8d4dbe4527b333ef0aa3
cydevPrateleirainfinita <josecydevprateleirainfinita@gmail.com>
2024-03-20 16:42:28 -0300

    Testes de execução de CI

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -3,7 +3,7 @@ if (!function_exists('onUpdateCurrentTimestamp')) {
     function onUpdateCurrentTimestamp()
     {
         return DB::connection()->getDriverName() !== 'sqlite'
-            ? DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+            ? DB::raw('CURRENT_TIMESTAMP')
             : null;
     }
 }
--- database/migrations/2023_08_22_115651_purchase_requests_log.php
+++ database/migrations/2023_08_22_115651_purchase_requests_log.php
@@ -7,16 +7,6 @@ return new class extends Migration
 {
     public function up(): void
     {
-        // Schema::create('purchase_requests_log', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->unsignedInteger('purchase_request_id');
-        //     $table->unsignedInteger('user_id');
-        //     $table->enum('action', ['create', 'update', 'delete']);
-        //     $table->dateTime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-        //     $table->dateTime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
-
-        //     $table->jsonb('changes')->nullable();
-        // });
         Schema::create('purchase_requests_log', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('purchase_request_id');
