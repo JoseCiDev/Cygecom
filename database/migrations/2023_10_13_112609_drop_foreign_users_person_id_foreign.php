3bc728995103b679020d1e23bd8216359e99d06b
cydevPrateleirainfinita <josecydevprateleirainfinita@gmail.com>
2024-03-20 16:13:26 -0300

    Testes de execução de CI

--- database/migrations/2023_10_13_112609_drop_foreign_users_person_id_foreign.php
+++ database/migrations/2023_10_13_112609_drop_foreign_users_person_id_foreign.php
@@ -9,14 +9,14 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            // $table->dropForeign('users_person_id_foreign');
+            $table->dropForeign('users_person_id_foreign');
         });
     }
 
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            // $table->foreign('person_id')->references('id')->on('people');
+            $table->foreign('person_id')->references('id')->on('people');
         });
     }
 };
