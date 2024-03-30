e3b6c72cfa1f9932bb38903ab19eb511e5f0ab72
JoseCiDev <josecydev@gmail.com>
2024-03-30 14:36:11 -0300

    Configurando CI

--- cypress/support/commands/commandsLogin.ts
+++ cypress/support/commands/commandsLogin.ts
@@ -121,7 +121,7 @@ const {
 Cypress.Commands.add('login', (emailAccess: string, passwordAccess: string, elementError: string) => {
 
     // cy.visit('https://gecom.essentia.com.br/login');
-    cy.log('Teste')
+   
 
     // cy.get(email, { timeout: 20000 })
     //     .each(($input) => {
