10be0a9f36fa0ffb858645292daed4db5861dc72
cidev3 <josecidev3@gmail.com>
2024-05-22 10:58:23 -0300

    Criado novo arquivo cy.ts chamado test e executando npm run e2e a partir desse arquivo.

---
+++ cypress/e2e/test.cy.ts
@@ -0,0 +1,110 @@
+/// <reference types="cypress" />
+
+import { faker } from '@faker-js/faker';
+
+import { elements as el } from '../elements';
+import { env } from 'process';
+import { data } from 'cypress/types/jquery';
+import { DataParameters } from '../import';
+import { dataParameters } from '../DataParameters/dataParameters';
+
+
+
+
+
+
+export const {
+    email,
+    password,
+    access,
+    titleLogin,
+    messageContainer,
+
+} = el.Login;
+
+export const {
+    userProfile,
+    homeMenu,
+    logoGecom,
+    homeScreen,
+
+} = el.Start;
+
+export const {
+    logout,
+    optionsMenu,
+    menuReduced,
+    breadcumbHome,
+    breadcumbUser,
+    showQuantityRecords,
+    SearchRegisteredUser,
+    nextPage,
+    pagePrevious,
+
+} = el.Shared;
+
+export const {
+    registrationMenu,
+    registrationMenuReduced,
+    registrationUserSubMenu,
+    createNewUser,
+    username,
+    birthdateUser,
+    cpfCnpjUser,
+    phoneUser,
+    emailUser,
+    userPassword,
+    confirmUserPassword,
+    sectorUser,
+    optionUserSector,
+    optionSelectUserSector,
+    optionSelectedSectorUser,
+    userApprover,
+    optionUserApprover,
+    limitUserApproval,
+    centerPermittedCostUser,
+    selectAllAllowedCostCenterUser,
+    clearCenterPermittedCostUser,
+    saveUserRegistration,
+    cancelUserRegistration,
+    registrationSupplierSubMenu,
+    messageRequirementName,
+    messageRequirementCpfCnpj,
+    messageRequiredTelephone,
+} = el.Register;
+
+export const {
+    requestMenu,
+    newRequestSubMenu,
+    myRequestSubMenu,
+    requestGeneralSubMenu,
+
+} = el.Request;
+
+export const {
+    supplyMenu,
+    dashboardSubMenu,
+    productSubMenu,
+    serviceSubMenu,
+    contractSubMenu,
+
+} = el.Supply;
+
+
+
+describe('Testes da página Login.', () => {
+
+
+
+    beforeEach(function () {
+
+    })
+
+    it('Deve retornar um log', () => {
+        
+        cy.visit('https://gecom.essentia.com.br/login');
+    });
+
+
+})
+
