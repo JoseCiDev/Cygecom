44f237040ede03ff943d8318da536fbbab76786f
cidev3 <josecidev3@gmail.com>
2024-05-31 16:46:56 -0300

    Testes de integracao continua.31052024.164557.

--- cypress/e2e/productRequest.cy.ts
+++ cypress/e2e/productRequest.cy.ts
@@ -116,11 +116,11 @@ describe('Testes da página de criação de solicitação de produtos.', () => {
 
     it(`Solicitação de produtos`, () => {
 
-        cy.login('http://192.168.0.66:9402', 'gecom_admin@essentia.com.br', 'admin123', messageContainer)
+        cy.login('http://gerenciador-compras.docker.local:8085', 'gecom_admin@essentia.com.br', 'essadmin@2023', messageContainer)
             .then((result) => {
                 assert.exists(result.success, result.error)
             });
-        cy.visit('http://192.168.0.66:9402/dashboard')
+        cy.visit('http://gerenciador-compras.docker.local:8085')
 
         cy.get('.btn').click();
 
@@ -128,7 +128,7 @@ describe('Testes da página de criação de solicitação de produtos.', () => {
             '.main-nav > :nth-child(3) > .btn',
             '[data-cy="dropdown-solicitacoes-novas"]',
         ]);
-        cy.createRequest(RequestType.product);
+        // cy.createRequest(RequestType.product);
     });
 });
 
