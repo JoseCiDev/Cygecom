bfa4af4c15a5f75da168f65c87b290e0fddee54a
cidev3 <josecidev3@gmail.com>
2024-05-31 15:05:15 -0300

    Testes de integracao continua 310524.150515.

--- cypress/e2e/productRequest.cy.ts
+++ cypress/e2e/productRequest.cy.ts
@@ -118,7 +118,7 @@ describe('Testes da página de criação de solicitação de produtos.', () => {
       
         
         // cy.url().should('include', '/login');
-        cy.get('[data-cy="logo-gecom"]');
+        cy.reload(true);
         // cy.createRequest(RequestType.product);
     });
 });
