9b26bdc57ec12e4b20e036903e5ee821a298f36b
cidev3 <josecidev3@gmail.com>
2024-06-03 14:31:07 -0300

    Testes de integração continua.03062024.143107.

--- cypress/e2e/productRequest.cy.ts
+++ cypress/e2e/productRequest.cy.ts
@@ -123,7 +123,7 @@ describe('Testes da página de criação de solicitação de produtos.', () => {
                 assert.exists(result.success, result.error)
             });
 
-        cy.visit('http://gerenciador-compras.docker.local:8085/users/edit/1');
+        // cy.visit('http://gerenciador-compras.docker.local:8085/requests/dashboard');
         cy.getElementAndClick([
             requestMenu,
             newRequestSubMenu,
