39492bfbaf808751908f16110d13a4832ba26f3e
cidev3 <josecidev3@gmail.com>
2024-06-11 17:51:13 -0300

    Configurando novo caminho para script e2e, configurado para ler toda a pasta.11062024.175113.

---
+++ cypress/e2e/productRequest.cy.ts
@@ -0,0 +1,111 @@
+/// <reference types="cypress" />
+
+import { faker } from '@faker-js/faker';
+
+import { elements as el } from '../elements';
+import { env } from 'process';
+import { data } from 'cypress/types/jquery';
+import { DataParameters } from '../import';
+import { dataParameters } from '../DataParameters/dataParameters';
+
+
+
+
+
+
+export const {
+    email,
+    password,
+    access,
+    titleLogin,
+    messageContainer,
+
+} = el.Login;
+
+export const {
+    userProfile,
+    homeMenu,
+    logoGecom,
+    homeScreen,
+
+} = el.Start;
+
+export const {
+    logout,
+    optionsMenu,
+    menuReduced,
+    breadcumbHome,
+    breadcumbUser,
+    showQuantityRecords,
+    SearchRegisteredUser,
+    nextPage,
+    pagePrevious,
+
+} = el.Shared;
+
+export const {
+    registrationMenu,
+    registrationMenuReduced,
+    registrationUserSubMenu,
+    createNewUser,
+    username,
+    birthdateUser,
+    cpfCnpjUser,
+    phoneUser,
+    emailUser,
+    userPassword,
+    confirmUserPassword,
+    sectorUser,
+    optionUserSector,
+    optionSelectUserSector,
+    optionSelectedSectorUser,
+    userApprover,
+    optionUserApprover,
+    limitUserApproval,
+    centerPermittedCostUser,
+    selectAllAllowedCostCenterUser,
+    clearCenterPermittedCostUser,
+    saveUserRegistration,
+    cancelUserRegistration,
+    registrationSupplierSubMenu,
+    messageRequirementName,
+    messageRequirementCpfCnpj,
+    messageRequiredTelephone,
+} = el.Register;
+
+export const {
+    requestMenu,
+    newRequestSubMenu,
+    myRequestSubMenu,
+    requestGeneralSubMenu,
+
+} = el.Request;
+
+export const {
+    supplyMenu,
+    dashboardSubMenu,
+    productSubMenu,
+    serviceSubMenu,
+    contractSubMenu,
+
+} = el.Supply;
+
+const environment = Cypress.env('ENVIRONMENT');
+const dataEnvironment = Cypress.env(environment);
+
+describe('Testes da página Login.', () => {
+
+
+
+    beforeEach(function () {
+
+    })
+
+    it('Deve efetuar o login com sucesso.', () => {
+        cy.login('http://gerenciador-compras.docker.local:8085', 'gecom_admin@essentia.com.br', 'essadmin@2023', messageContainer)
+            .then((result) => {
+                assert.exists(result.success, result.error)
+            });
+        
+    });
+});
